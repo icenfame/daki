4eb8ec47ad05235dfcf7eb13db78c1b8455e0d4e
Plombir4ik <plybomur720@gmail.com>
2021-10-31 22:39:01 +0200

    Edit screen finish

--- src/screens/Edit/Index.jsx
+++ src/screens/Edit/Index.jsx
@@ -1,12 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Image, View, Platform } from 'react-native';
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
+import {
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  Platform,
+  ImageBackground
+} from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import { Feather, Ionicons } from "@expo/vector-icons"
 import * as ImagePicker from 'expo-image-picker';
+import Constants from "expo-constants";
 import uuid from 'uuid';
+import "moment/locale/uk";
+import Moment from "react-moment";
+
+// Styles
+import styles from "./styles";
+// Firebase
 import { firebase, db, auth } from "../../firebase";
 
-
-export default function ImagePickerExample() {
+export default function EditScreen({ route, navigation }) {
   const [image, setImage] = useState(null);
+  const [photo, setPhoto] = useState(route.params.profile_photo);
+  const [newName, setNewName] = useState("");
+  const [newBio, setNewBio] = useState("");
+
 
   useEffect(() => {
     (async () => {
@@ -19,58 +41,109 @@ export default function ImagePickerExample() {
     })();
   }, []);
 
+  useLayoutEffect(() => {
+    navigation.setOptions({
+        headerTitle: "Редагувати",
+        headerRight: () => (
+          <TouchableOpacity onPress = {() => updateInfo(image)}>
+            <Ionicons name="checkmark-sharp" size={26} color="#000" />
+          </TouchableOpacity>
+        ),
+    });
+  });
+
+
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     });
 
-    console.log(result);
+    //console.log(result);
 
     if (!result.cancelled) {
       setImage(result.uri);
+      setPhoto(result.uri);
     }
   };
+  
+  function updateInfo(uri){
+    if(uri != null) uploadImageAsync(uri); else{
+      db.collection("users").doc(auth.currentUser?.uid).update({
+        name : newName,
+        bio : newBio,
+      });
+      navigation.goBack();
+    }
+  }
+
+  async function uploadImageAsync(uri) {
+    const blob = await new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.onload = function () {
+            resolve(xhr.response);
+        };
+        xhr.onerror = function (e) {
+            console.log(e);
+            reject(new TypeError('Network request failed'));
+        };
+        xhr.responseType = 'blob';
+        xhr.open('GET', uri, true);
+        xhr.send(null);
+    });
+  
+    const ref = firebase
+        .storage()
+        .ref()
+        .child(uuid.v4());
+    const snapshot = await ref.put(blob);
+  
+    // We're done with the blob, close and release it
+    blob.close();
+  
+    let url = await snapshot.ref.getDownloadURL();
+
+    db.collection("users").doc(auth.currentUser?.uid).update({
+      name : newName,
+      bio : newBio,
+      profilePhoto: url
+    });
+
+    navigation.goBack();
+    return url;
+    
+  }
+  
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      <Button title="GO" onPress={() => uploadImageAsync(image)} />
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <View flexDirection="row" style = {{padding : 15}}>
+
+        <TouchableOpacity onPress = {pickImage}>
+
+          <ImageBackground source={image != null ? {uri : image} : {uri : route.params.userPhoto} } style={styles.profile_photo} imageStyle={{ borderRadius: 40}}>
+            <Feather
+            name = "camera"
+            size = {32}
+            style = {{backgroundColor: "rgba(211, 211, 211, 0.5)", padding: 19, borderRadius : 40}}
+            />
+          </ImageBackground>
+
+        </TouchableOpacity>
+
+        <View style={{marginLeft: 10}}>
+          <TextInput 
+          placeholder = "Ім'я" 
+          style = {styles.input} onChangeText = {(name) => setNewName(name)} defaultValue = {route.params.userName} />
+
+          <TextInput 
+          placeholder = "Про себе" 
+          style = {[styles.input, {marginTop: 5}]} onChangeText = {(bio) => setNewBio(bio)} defaultValue = {route.params.userBio} />
+        </View>
+      </View>
     </View>
   );
-}
-
-
-async function uploadImageAsync(uri) {
-  // Why are we using XMLHttpRequest? See:
-  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
-  const blob = await new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-          resolve(xhr.response);
-      };
-      xhr.onerror = function (e) {
-          console.log(e);
-          reject(new TypeError('Network request failed'));
-      };
-      xhr.responseType = 'blob';
-      xhr.open('GET', uri, true);
-      xhr.send(null);
-  });
-
-  const ref = firebase
-      .storage()
-      .ref()
-      .child(uuid.v4());
-  const snapshot = await ref.put(blob);
-
-  // We're done with the blob, close and release it
-  blob.close();
-
-  let url = await snapshot.ref.getDownloadURL();
-  console.log("Вот такой фот урл получился! ", url)
-  return url;
 }

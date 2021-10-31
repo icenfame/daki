4ed6cf73fbc408271fc20d27bc7229e7fafcd7a8
Plombir4ik <plybomur720@gmail.com>
2021-10-31 20:42:21 +0200

    Image attach test

--- src/screens/Edit/Index.jsx
+++ src/screens/Edit/Index.jsx
@@ -1,80 +1,76 @@
-import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
-import {
-  Text,
-  View,
-  Dimensions,
-  TouchableOpacity,
-  TextInput,
-  Image,
-  ImageBackground,
-} from "react-native";
-
-import { StatusBar } from "expo-status-bar";
-import { Feather, Ionicons } from "@expo/vector-icons";
-import "moment/locale/uk";
-import Moment from "react-moment";
-
-// Styles
-import styles from "./styles";
-// Firebase
+import React, { useState, useEffect } from 'react';
+import { Button, Image, View, Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import uuid from 'uuid';
 import { firebase, db, auth } from "../../firebase";
 
-export default function EditScreen({ route, navigation }) {
-  //   useEffect(() => {
-  //     // DB
 
-  //   }, []);
+export default function ImagePickerExample() {
+  const [image, setImage] = useState(null);
 
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerTitle: "Редагувати",
-      headerRight: () => (
-        <TouchableOpacity onPress={() => console.log(route)}>
-          <Ionicons name="checkmark-sharp" size={26} color="#000" />
-        </TouchableOpacity>
-      ),
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
     });
-  });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View flexDirection="row" style={{ padding: 15 }}>
-        <TouchableOpacity>
-          <ImageBackground
-            source={{ uri: route.params.userPhoto }}
-            style={styles.profile_photo}
-            imageStyle={{ borderRadius: 40 }}
-          >
-            <Feather
-              name="camera"
-              size={32}
-              style={{
-                backgroundColor: "rgba(211, 211, 211, 0.5)",
-                padding: 19,
-                borderRadius: 40,
-              }}
-            />
-          </ImageBackground>
-        </TouchableOpacity>
-
-        <View style={{ marginLeft: 10 }}>
-          <TextInput
-            // placeholder = "Ім'я"
-            style={styles.input}
-            editable={true}
-            defaultValue={route.params.userName}
-            selectionColor="#000"
-          />
-
-          <TextInput
-            // placeholder = "Про себе"
-            style={[styles.input, { marginTop: 5 }]}
-            defaultValue={route.params.userBio}
-            selectionColor="#000"
-          />
-        </View>
-      </View>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      <Button title="GO" onPress={() => uploadImageAsync(image)} />
     </View>
   );
+}
+
+
+async function uploadImageAsync(uri) {
+  // Why are we using XMLHttpRequest? See:
+  // https://github.com/expo/expo/issues/2402#issuecomment-443726662
+  const blob = await new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+          resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+          console.log(e);
+          reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      xhr.open('GET', uri, true);
+      xhr.send(null);
+  });
+
+  const ref = firebase
+      .storage()
+      .ref()
+      .child(uuid.v4());
+  const snapshot = await ref.put(blob);
+
+  // We're done with the blob, close and release it
+  blob.close();
+
+  let url = await snapshot.ref.getDownloadURL();
+  console.log("Вот такой фот урл получился! ", url)
+  return url;
 }

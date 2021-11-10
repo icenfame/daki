b4563ee7b6f0c45644ec1f260bffd4c2e31b2a45
Dragon Flip <62544653+dragonflip@users.noreply.github.com>
2021-11-10 23:02:07 +0200

    Added loading indicator for group info and user info

--- src/screens/Chats/GroupInfo.jsx
+++ src/screens/Chats/GroupInfo.jsx
@@ -6,6 +6,7 @@ import {
   TouchableOpacity,
   Image,
   FlatList,
+  ActivityIndicator,
 } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
@@ -21,6 +22,7 @@ import { firebase, db, auth } from "../../firebase";
 export default function ChatGroupInfoScreen({ route, navigation }) {
   const [groupInfo, setGroupInfo] = useState([]);
   const [members, setMembers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   // Init
   useEffect(() => {
@@ -33,6 +35,7 @@ export default function ChatGroupInfoScreen({ route, navigation }) {
           ...snapshot.data(),
           membersCount: snapshot.data().members.length,
         });
+        setLoading(false);
       });
 
     const membersSnapshotUnsubscribe = db
@@ -61,172 +64,184 @@ export default function ChatGroupInfoScreen({ route, navigation }) {
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <FlatList
-        data={members}
-        keyExtractor={(item) => item.id}
-        ListHeaderComponent={() => (
-          <View>
-            {groupInfo.groupPhoto !== "" ? (
-              <Image
-                source={{
-                  uri: groupInfo.groupPhoto,
-                }}
-                style={{
-                  width: 192,
-                  height: 192,
-                  borderRadius: 192,
-                  alignSelf: "center",
-                  marginTop: 64,
-                }}
-              />
-            ) : (
-              <View
-                style={{
-                  width: Dimensions.get("window").width,
-                  height: Dimensions.get("window").width,
-                  alignItems: "center",
-                  justifyContent: "center",
-                  backgroundColor: "#eee",
-                }}
-              >
-                <Ionicons
-                  name="camera"
-                  size={Dimensions.get("window").width * 0.4}
-                  color="#aaa"
-                />
-              </View>
-            )}
-
+      {!loading ? (
+        <FlatList
+          data={members}
+          keyExtractor={(item) => item.id}
+          ListHeaderComponent={() => (
             <View>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  paddingTop: 16,
-                }}
-              >
-                <View style={{ flex: 1, paddingBottom: 16 }}>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <Text style={{ fontSize: 24 }}>{groupInfo.groupName}</Text>
-                  </View>
+              {groupInfo.groupPhoto !== "" ? (
+                <Image
+                  source={{
+                    uri: groupInfo.groupPhoto,
+                  }}
+                  style={{
+                    width: 192,
+                    height: 192,
+                    borderRadius: 192,
+                    alignSelf: "center",
+                    marginTop: 64,
+                  }}
+                />
+              ) : (
+                <View
+                  style={{
+                    width: Dimensions.get("window").width,
+                    height: Dimensions.get("window").width,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#eee",
+                  }}
+                >
+                  <Ionicons
+                    name="camera"
+                    size={Dimensions.get("window").width * 0.4}
+                    color="#aaa"
+                  />
+                </View>
+              )}
 
-                  <Text style={{ color: "grey", textAlign: "center" }}>
-                    Учасників: {groupInfo.membersCount}
+              <View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    paddingTop: 16,
+                  }}
+                >
+                  <View style={{ flex: 1, paddingBottom: 16 }}>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      <Text style={{ fontSize: 24 }}>
+                        {groupInfo.groupName}
+                      </Text>
+                    </View>
+
+                    <Text style={{ color: "grey", textAlign: "center" }}>
+                      Учасників: {groupInfo.membersCount}
+                    </Text>
+                  </View>
+                </View>
+
+                <TouchableOpacity
+                  onPress={() => navigation.goBack()}
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "#eee",
+                    borderRadius: 16,
+                    paddingVertical: 12,
+                    paddingHorizontal: 16,
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <MaterialCommunityIcons name="send" size={14} color="blue" />
+                  <Text style={{ color: "blue", fontSize: 14, marginLeft: 4 }}>
+                    Написати повідомлення
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "#eee",
+                    borderRadius: 16,
+                    paddingVertical: 12,
+                    paddingHorizontal: 16,
+                    marginTop: 8,
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <MaterialCommunityIcons name="logout" size={14} color="red" />
+                  <Text style={{ color: "red", fontSize: 14, marginLeft: 4 }}>
+                    Вийти з групи
+                  </Text>
+                </TouchableOpacity>
+
+                <Text
+                  style={{ fontSize: 20, fontWeight: "300", marginTop: 16 }}
+                >
+                  Учасники
+                </Text>
+              </View>
+            </View>
+          )}
+          contentContainerStyle={{ padding: 16 }}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                borderBottomWidth: 1,
+                borderColor: "#eee",
+                padding: 8,
+                alignItems: "center",
+              }}
+              activeOpacity={0.5}
+              onPress={() =>
+                navigation.navigate("ChatsUserInfo", { userId: item.id })
+              }
+            >
+              {item.profilePhoto !== "" ? (
+                <Image
+                  source={{ uri: item.profilePhoto }}
+                  style={{ width: 48, height: 48, borderRadius: 48 }}
+                />
+              ) : (
+                <View
+                  style={{
+                    width: 48,
+                    height: 48,
+                    borderRadius: 48,
+                    backgroundColor: "#aaa",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 18, color: "#fff" }}>
+                    {item.name[0]}
                   </Text>
                 </View>
-              </View>
-
-              <TouchableOpacity
-                onPress={() => navigation.goBack()}
-                style={{
-                  borderWidth: 1,
-                  borderColor: "#eee",
-                  borderRadius: 16,
-                  paddingVertical: 12,
-                  paddingHorizontal: 16,
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-              >
-                <MaterialCommunityIcons name="send" size={14} color="blue" />
-                <Text style={{ color: "blue", fontSize: 14, marginLeft: 4 }}>
-                  Написати повідомлення
-                </Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                style={{
-                  borderWidth: 1,
-                  borderColor: "#eee",
-                  borderRadius: 16,
-                  paddingVertical: 12,
-                  paddingHorizontal: 16,
-                  marginTop: 8,
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-              >
-                <MaterialCommunityIcons name="logout" size={14} color="red" />
-                <Text style={{ color: "red", fontSize: 14, marginLeft: 4 }}>
-                  Вийти з групи
-                </Text>
-              </TouchableOpacity>
-
-              <Text style={{ fontSize: 20, fontWeight: "300", marginTop: 16 }}>
-                Учасники
-              </Text>
-            </View>
-          </View>
-        )}
-        contentContainerStyle={{ padding: 16 }}
-        renderItem={({ item }) => (
-          <TouchableOpacity
-            style={{
-              flexDirection: "row",
-              borderBottomWidth: 1,
-              borderColor: "#eee",
-              padding: 8,
-              alignItems: "center",
-            }}
-            activeOpacity={0.5}
-            onPress={() =>
-              navigation.navigate("ChatsUserInfo", { userId: item.id })
-            }
-          >
-            {item.profilePhoto !== "" ? (
-              <Image
-                source={{ uri: item.profilePhoto }}
-                style={{ width: 48, height: 48, borderRadius: 48 }}
-              />
-            ) : (
-              <View
-                style={{
-                  width: 48,
-                  height: 48,
-                  borderRadius: 48,
-                  backgroundColor: "#aaa",
-                  alignItems: "center",
-                  justifyContent: "center",
-                }}
-              >
-                <Text style={{ fontSize: 18, color: "#fff" }}>
-                  {item.name[0]}
-                </Text>
-              </View>
-            )}
-            <View style={{ marginLeft: 8, flex: 1 }}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                }}
-              >
-                <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
-                {item.admin ? (
-                  <Text style={{ color: "blue", fontSize: 12 }}>адмін</Text>
-                ) : null}
-              </View>
-
-              {item.online.seconds >
-              firebase.firestore.Timestamp.now().seconds + 10 ? (
-                <Text style={{ color: "green", fontSize: 12 }}>онлайн</Text>
-              ) : (
-                <Text style={{ color: "grey", fontSize: 12 }}>
-                  в мережі{" "}
-                  <Moment element={Text} locale="uk" fromNow unix>
-                    {item.online?.seconds}
-                  </Moment>
-                </Text>
               )}
-            </View>
-          </TouchableOpacity>
-        )}
-      />
+              <View style={{ marginLeft: 8, flex: 1 }}>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+                  {item.admin ? (
+                    <Text style={{ color: "blue", fontSize: 12 }}>адмін</Text>
+                  ) : null}
+                </View>
+
+                {item.online.seconds >
+                firebase.firestore.Timestamp.now().seconds + 10 ? (
+                  <Text style={{ color: "green", fontSize: 12 }}>онлайн</Text>
+                ) : (
+                  <Text style={{ color: "grey", fontSize: 12 }}>
+                    в мережі{" "}
+                    <Moment element={Text} locale="uk" fromNow unix>
+                      {item.online?.seconds}
+                    </Moment>
+                  </Text>
+                )}
+              </View>
+            </TouchableOpacity>
+          )}
+        />
+      ) : (
+        <ActivityIndicator
+          color="#000"
+          style={{ flex: 1 }}
+          size={Platform.OS === "android" ? "large" : "small"}
+        />
+      )}
     </View>
   );
 }
--- src/screens/Chats/UserInfo.jsx
+++ src/screens/Chats/UserInfo.jsx
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import {
   Text,
   View,
-  Dimensions,
   TouchableOpacity,
   ScrollView,
   Image,
+  ActivityIndicator,
 } from "react-native";
 
 import { StatusBar } from "expo-status-bar";
-import { Octicons, Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import "moment/locale/uk";
 import Moment from "react-moment";
 import StarRating from "react-native-star-rating";
@@ -21,6 +21,7 @@ import { firebase, db } from "../../firebase";
 
 export default function ProfileScreen({ route, navigation }) {
   const [profile, setProfile] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [starRate, setStarRate] = useState(2.5);
 
   // Init
@@ -31,6 +32,7 @@ export default function ProfileScreen({ route, navigation }) {
       .doc(route.params.userId)
       .onSnapshot((snapshot) => {
         setProfile(snapshot.data());
+        setLoading(false);
       });
 
     return userSnapshotUnsubscribe;
@@ -40,81 +42,82 @@ export default function ProfileScreen({ route, navigation }) {
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <ScrollView>
-        {profile.profilePhoto !== "" ? (
-          <Image
-            source={{
-              uri: profile.profilePhoto,
-            }}
-            style={{
-              width: 192,
-              height: 192,
-              borderRadius: 192,
-              alignSelf: "center",
-              marginTop: 64,
-            }}
-          />
-        ) : (
-          <View
-            style={{
-              width: 192,
-              height: 192,
-              borderRadius: 192,
-              alignSelf: "center",
-              alignItems: "center",
-              justifyContent: "center",
-              marginTop: 64,
-              backgroundColor: "#eee",
-            }}
-          >
-            <Text style={{ fontSize: 48 }}>{profile.name[0]}</Text>
-          </View>
-        )}
+      {!loading ? (
+        <ScrollView>
+          {profile.profilePhoto !== "" ? (
+            <Image
+              source={{
+                uri: profile.profilePhoto,
+              }}
+              style={{
+                width: 192,
+                height: 192,
+                borderRadius: 192,
+                alignSelf: "center",
+                marginTop: 64,
+              }}
+            />
+          ) : (
+            <View
+              style={{
+                width: 192,
+                height: 192,
+                borderRadius: 192,
+                alignSelf: "center",
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: 64,
+                backgroundColor: "#eee",
+              }}
+            >
+              <Text style={{ fontSize: 48 }}>{profile.name[0]}</Text>
+            </View>
+          )}
 
-        <View style={{ paddingHorizontal: 16 }}>
-          <View
-            style={{
-              paddingTop: 16,
-              alignItems: "center",
-            }}
-          >
-            <View style={{ paddingBottom: 16, alignItems: "center" }}>
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                }}
-              >
-                <Text style={{ fontSize: 24 }}>{profile.name}</Text>
-                <Text
+          <View style={{ paddingHorizontal: 16 }}>
+            <View
+              style={{
+                paddingTop: 16,
+                alignItems: "center",
+              }}
+            >
+              <View style={{ paddingBottom: 16, alignItems: "center" }}>
+                <View
                   style={{
-                    fontSize: 20,
-                    fontWeight: "bold",
-                    color: "red",
-                    marginLeft: 8,
+                    flexDirection: "row",
+                    alignItems: "center",
                   }}
                 >
-                  2.4★
-                </Text>
-              </View>
-
-              {profile.online?.seconds >
-              firebase.firestore.Timestamp.now().seconds + 10 ? (
-                <Text style={{ color: "green" }}>онлайн</Text>
-              ) : (
-                <View>
-                  <Text style={{ color: "grey" }}>
-                    В мережі{" "}
-                    <Moment element={Text} locale="uk" fromNow unix>
-                      {profile.online?.seconds}
-                    </Moment>
+                  <Text style={{ fontSize: 24 }}>{profile.name}</Text>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: "bold",
+                      color: "red",
+                      marginLeft: 8,
+                    }}
+                  >
+                    2.4★
                   </Text>
                 </View>
-              )}
-            </View>
-          </View>
 
-          {/* <View style={{ flex: 1, paddingBottom: 16 }}>
+                {profile.online?.seconds >
+                firebase.firestore.Timestamp.now().seconds + 10 ? (
+                  <Text style={{ color: "green" }}>онлайн</Text>
+                ) : (
+                  <View>
+                    <Text style={{ color: "grey" }}>
+                      В мережі{" "}
+                      <Moment element={Text} locale="uk" fromNow unix>
+                        {profile.online?.seconds}
+                      </Moment>
+                    </Text>
+                  </View>
+                )}
+              </View>
+            </View>
+
+            {/* <View style={{ flex: 1, paddingBottom: 16 }}>
             <View style={{ flexDirection: "row", alignItems: "center" }}>
               <StarRating
                 disabled={false}
@@ -125,7 +128,7 @@ export default function ProfileScreen({ route, navigation }) {
             </View>
           </View> */}
 
-          {/* <View
+            {/* <View
             style={{
               paddingVertical: 16,
               borderColor: "#eee",
@@ -145,74 +148,87 @@ export default function ProfileScreen({ route, navigation }) {
             <Text style={{ fontSize: 12, color: "grey" }}>Рейтинг</Text>
           </View> */}
 
-          <View
-            style={{
-              paddingVertical: 16,
-              borderColor: "#eee",
-              borderTopWidth: 1,
-            }}
-          >
-            <Text
+            <View
               style={{
-                fontSize: 16,
-                fontWeight: "bold",
-                textTransform: "uppercase",
+                paddingVertical: 16,
+                borderColor: "#eee",
+                borderTopWidth: 1,
               }}
             >
-              {profile.phone}
-            </Text>
-            <Text style={{ fontSize: 12, color: "grey" }}>Номер телефону</Text>
-          </View>
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: "bold",
+                  textTransform: "uppercase",
+                }}
+              >
+                {profile.phone}
+              </Text>
+              <Text style={{ fontSize: 12, color: "grey" }}>
+                Номер телефону
+              </Text>
+            </View>
 
-          <View
-            style={{
-              paddingVertical: 16,
-              borderColor: "#eee",
-              borderTopWidth: 1,
-            }}
-          >
-            <Text style={{ fontSize: 16 }}>
-              {profile.bio != "" ? profile.bio : "Розкажіть про себе"}
-            </Text>
-            <Text style={{ fontSize: 12, color: "grey" }}>Про себе</Text>
-          </View>
+            <View
+              style={{
+                paddingVertical: 16,
+                borderColor: "#eee",
+                borderTopWidth: 1,
+              }}
+            >
+              <Text style={{ fontSize: 16 }}>
+                {profile.bio != "" ? profile.bio : "Розкажіть про себе"}
+              </Text>
+              <Text style={{ fontSize: 12, color: "grey" }}>Про себе</Text>
+            </View>
 
-          <TouchableOpacity
-            onPress={() => navigation.goBack()}
-            style={{
-              borderWidth: 1,
-              borderColor: "#eee",
-              borderRadius: 16,
-              paddingVertical: 12,
-              paddingHorizontal: 16,
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <MaterialCommunityIcons name="send" size={16} color="blue" />
-            <Text style={{ color: "blue", fontSize: 16, marginLeft: 4 }}>
-              Написати повідомлення
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={{
-              borderWidth: 1,
-              borderColor: "#eee",
-              borderRadius: 16,
-              paddingVertical: 12,
-              paddingHorizontal: 16,
-              marginTop: 8,
-              flexDirection: "row",
-              alignItems: "center",
-            }}
-          >
-            <MaterialCommunityIcons name="block-helper" size={16} color="red" />
-            <Text style={{ color: "red", fontSize: 16, marginLeft: 4 }}>
-              Заблокувати
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={{
+                borderWidth: 1,
+                borderColor: "#eee",
+                borderRadius: 16,
+                paddingVertical: 12,
+                paddingHorizontal: 16,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <MaterialCommunityIcons name="send" size={16} color="blue" />
+              <Text style={{ color: "blue", fontSize: 16, marginLeft: 4 }}>
+                Написати повідомлення
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={{
+                borderWidth: 1,
+                borderColor: "#eee",
+                borderRadius: 16,
+                paddingVertical: 12,
+                paddingHorizontal: 16,
+                marginTop: 8,
+                flexDirection: "row",
+                alignItems: "center",
+              }}
+            >
+              <MaterialCommunityIcons
+                name="block-helper"
+                size={16}
+                color="red"
+              />
+              <Text style={{ color: "red", fontSize: 16, marginLeft: 4 }}>
+                Заблокувати
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      ) : (
+        <ActivityIndicator
+          color="#000"
+          style={{ flex: 1 }}
+          size={Platform.OS === "android" ? "large" : "small"}
+        />
+      )}
     </View>
   );
 }
